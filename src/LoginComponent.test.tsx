e348f522d82cea8af24ea93f6a2944419405b167
Fiqri Dwi <nurfiqri23@gmail.com>
2023-07-31 15:48:01 +0700

    feat: add event triggered test

--- src/LoginComponent.test.tsx
+++ src/LoginComponent.test.tsx
@@ -1,5 +1,7 @@
 import LoginComponent from "./LoginComponent";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import { act } from "@testing-library/react";
 
 describe("Login component tests", () => {
 	const loginServiceMock = {
@@ -9,7 +11,9 @@ describe("Login component tests", () => {
 
 	let container: HTMLElement;
 	function setup() {
-		container = render(<LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />).container;
+		container = render(
+			<LoginComponent loginService={loginServiceMock} setToken={setTokenMock} />
+		).container;
 	}
 
 	beforeEach(() => {
@@ -22,11 +26,31 @@ describe("Login component tests", () => {
 		expect(screen.queryByTestId("resutLabel")).not.toBeInTheDocument();
 	});
 	it("should render correctly - query by test id", () => {
-		// const inputs = screen.getAllByTestId("input");
-		const inputs = container.querySelectorAll("input");
+		const inputs = screen.getAllByTestId("input");
+		// const inputs = container.querySelectorAll("input");
 		expect(inputs).toHaveLength(3);
 		expect(inputs[0].getAttribute("value")).toBe("");
 		expect(inputs[1].getAttribute("value")).toBe("");
 		expect(inputs[2].getAttribute("value")).toBe("Login");
+	});
+	it("Click login button with incomplete credentials - show required message", () => {
+		const inputs = screen.getAllByTestId("input");
+		const loginButton = inputs[2];
+
+		fireEvent.click(loginButton);
+
+		const resultLabel = screen.getByTestId("resultLabel");
+		expect(resultLabel.textContent).toBe("UserName and password required!");
+	});
+	it("Click login button with incomplete credentials - show required message - with user click", () => {
+		const inputs = screen.getAllByTestId("input");
+		const loginButton = inputs[2];
+
+		act(() => {
+			user.click(loginButton);
+		});
+
+		const resultLabel = screen.getByTestId("resultLabel");
+		expect(resultLabel.textContent).toBe("UserName and password required!");
 	});
 });
